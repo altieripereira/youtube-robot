a7922d8f66ab2c92f9b52863b7ea0682877e8026
altieri <altieritga@gmail.com>
2021-03-21 22:57:16 -0400

    feat: add watson natural language understanding service

--- credentials/readm.md
+++ credentials/readm.md
@@ -8,3 +8,17 @@ File 'algorighmia.json'
     "apiKey": "PUBLIC API KEY"
 }
 ```
+
+## Watson Natural Language Undertanding
+File: 'watson-nlu.json'
+
+```
+  {
+    "apikey": "",
+    "iam_apikey_description": "",
+    "iam_apikey_name": "",
+    "iam_role_crn": "",
+    "iam_serviceid_crn": "",
+    "url": ""
+  }
+```
--- robots/text.js
+++ robots/text.js
@@ -2,10 +2,22 @@ const algorithmia = require('algorithmia')
 const sentenceBoundaryDetection = require('sbd')
 const algorithmiaApiKey = require('../credentials/algorithmia.json').apiKey
 
+const watsonApiKey = require('../credentials/watson-nlu.json').apikey
+const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js')
+ const nlu = new NaturalLanguageUnderstandingV1({
+  iam_apikey: watsonApiKey,
+  version: '2018-04-05',
+  url: 'https://gateway.watsonplatform.net/natural-language-understanding/api/'
+})
+
 async function robot(content) {
+
     await fetchContentFromWikipedia(content)
     sanitizeContent(content)
     breakContentIntoSentences(content)
+    limitMaximumSentences(content)
+
+    await fetchKeywordsOfAllSentences(content)
 
     async function fetchContentFromWikipedia(content) {
 
@@ -26,7 +38,6 @@ async function robot(content) {
 
         const withoutBlankAndMarkdownLines = removeBlankLInesAndMarkdown(content.sourceContentOrignal)
         const withoutDatesInParentheses  = removeDatesInParentheses(withoutBlankAndMarkdownLines)
-
         content.sourceContentSanitized = withoutDatesInParentheses
     }
 
@@ -47,17 +58,50 @@ async function robot(content) {
     }
 
     function breakContentIntoSentences(content) {
-
-        content.senteces = []
+        content.sentences = []
         const sentences = sentenceBoundaryDetection.sentences(content.sourceContentSanitized)
+
         sentences.forEach((sentence) => {
-            content.senteces.push({
+            content.sentences.push({
                 text: sentence, 
-                keyWords: [],
+                keywords: [],
                 images: [] 
             })
         })
     }
+
+    function limitMaximumSentences(content) {
+        content.sentences = content.sentences.slice(0, content.maximumSentences)
+    }
+
+    async function fetchKeywordsOfAllSentences(content) {
+
+        for (const sentence of content.sentences) {
+            sentence.keywords = await fetchWatsonAndReturnKeywords(sentence.text)
+        }
+    }
+
+    async function fetchWatsonAndReturnKeywords(sentence) {
+        return new Promise((resolve, reject) => {
+          nlu.analyze({
+            text: sentence,
+            features: {
+              keywords: {}
+            }
+          }, (error, response) => {
+            if (error) {
+              reject(error)
+              return
+            }
+    
+            const keywords = response.keywords.map((keyword) => {
+              return keyword.text
+            })
+    
+            resolve(keywords)
+          })
+        })
+      }
 }
 
 module.exports = robot
